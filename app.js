056a8f164cf461d372182544f0b13f804bb7c7e5
Abhishek ghosh <shekghoshabhi@gmail.com>
2021-09-18 20:58:46 +0530

    bsic

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 const port = 3000 ;
 
+const mongoos = require('mongoose')
+const link = 'mongodb+srv://admin:admin@cluster0.j6t63.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoos.connect(link)
+.then((db)=>{
+    console.log('mongoose connected')
+}).catch(err=>{
+    console.log(err)
+})
+
 
 app.get('/',(req,res)=>{
     res.send("send ")
